ae7a02ef421fcc3a1111baf92bf037b79349f97f
Sweet coderr <89229676+ChokoNetZ@users.noreply.github.com>
2021-10-17 19:49:08 -0400

    Add files via upload

---
+++ Emailautomático.py
@@ -0,0 +1,26 @@
+import json
+import os
+import time 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+import smtplib 
+# Corpo da mensagem do email 
+msg = MIMEMultipart()  
+# Credenciais e assunto do email # 
+print('Carregando...')
+time.sleep(1)
+print('Carregado com sucesso!')
+time.sleep(2)
+os.system("clear")
+print('=== Enviador de gmail com python ===')
+msg['From'] = input("Insira seu email: ")
+password = input("Insira a senha do seu Gmail: ")
+msg['To'] = input("Insira o destinatário: ") 
+msg['Subject'] = input("Insira o título do email: ")
+message = input("Insira a mensagem que você deseja enviar: ") 
+msg.attach(MIMEText (message, "plain"))
+server = smtplib.SMTP('smtp.gmail.com', port=587)
+server.starttls() 
+server.login(msg['From'], password) 
+server.sendmail(msg['From'], msg['To'], msg.as_string()) 
+server.quit()
