7e71129330f8840b699a05ceb488414692e46ba2
Skaskya <89229676+Skaskya@users.noreply.github.com>
2021-11-29 19:25:22 -0400

    Update and rename Emailautomático.py to EZGmail.py

--- EZGmail.py
+++ EZGmail.py
@@ -6,19 +6,22 @@ from email.mime.text import MIMEText
 import smtplib 
 # Corpo da mensagem do email 
 msg = MIMEMultipart()  
-# Credenciais e assunto do email # 
+#detalhes descartaveis
 print('Carregando...')
-time.sleep(1)
+time.sleep(0.1)
 print('Carregado com sucesso!')
-time.sleep(2)
+time.sleep(0.1)
 os.system("clear")
+#
 print('=== Enviador de gmail com python ===')
+# Credenciais e assunto do email 
 msg['From'] = input("Insira seu email: ")
 password = input("Insira a senha do seu Gmail: ")
 msg['To'] = input("Insira o destinatário: ") 
 msg['Subject'] = input("Insira o título do email: ")
 message = input("Insira a mensagem que você deseja enviar: ") 
 msg.attach(MIMEText (message, "plain"))
+#port
 server = smtplib.SMTP('smtp.gmail.com', port=587)
 server.starttls() 
 server.login(msg['From'], password) 
